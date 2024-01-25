61b856cc0137cc37396c2e64e7ce0ee5ac7d7308
David-Kuts <david.mw.kuts@gmail.com>
2024-01-26 01:24:34 +0500

    Add soulution of Chapter05_Exercise_005

---
+++ Chapter05_Exercise_005/Exercise_005.cpp
@@ -0,0 +1,23 @@
+// Exercise_005.cpp
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string months[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+	int* p_monthlySalesAmounts = new int [12];
+
+	std::cout << "Enter the sales amount for:" << std::endl;
+
+	int totalSoldBookAmouts = 0;
+
+	for (int i = 0; i < 12; i++) {
+		std::cout << months[i] << ": ";
+		std::cin >> p_monthlySalesAmounts[i];
+
+		totalSoldBookAmouts += p_monthlySalesAmounts[i];
+	}
+
+	std::cout << "Tatal amout of the sold books:" << totalSoldBookAmouts << std::endl;
+
+	return 0;
+}
