b08f8a5836f2bd5141e48d472dc5913341a36506
David-Kuts <david.mw.kuts@gmail.com>
2024-01-28 17:07:45 +0500

    Update the solution Chapter06_Exercise_005

--- Chapter06_Exercise_005/Exercise_005.cpp
+++ Chapter06_Exercise_005/Exercise_005.cpp
@@ -10,44 +10,29 @@ int main() {
 	std::cout << "Enter income: ";
 
 	while (std::cin >> income) {
+		tax = 0;
+
 		if (income >= 0) {
 			if (income > 35000) {
-				taxRangeIndex = 4;
+				tax += (income - 35000) * 0.2;
+				income = 35000;
 			}
-			else if (income > 20000) {
-				taxRangeIndex = 3;
+			if (income > 20000) {
+				tax += (income - 20000) * 0.15;
+				income = 20000;
 			}
-			else if (income > 10000) {
-				taxRangeIndex = 2;
+			if (income > 10000) {
+				tax += (income - 10000) * 0.10;
+				income = 10000;
 			}
-			else if (income > 5000) {
-				taxRangeIndex = 1;
+			if (income > 5000) {
+				tax += (income - 5000) * 0.05;
 			}
-			else {
-				taxRangeIndex = 0;
-			}
-
-			switch (taxRangeIndex) {
-				case 4:
-					tax += (income - 35000) * 0.2;
-					income = 35000;
-				case 3:
-					tax += (income - 20000) * 0.15;
-					income = 20000;
-				case 2:
-					tax += (income - 10000) * 0.10;
-					income = 10000;
-				case 1:
-					tax += (income - 5000) * 0.05;
-				case 0:
-					std::cout << "Tax: " << tax << std::endl;
-					std::cout << "Enter income: ";
-
-					tax = 0;
-			}
+			
+			std::cout << "Tax: " << tax << std::endl;
+			std::cout << "Enter income: ";
 		}
 		else {
-			std::cout << "Bye";
 			break;
 		}
 	}
