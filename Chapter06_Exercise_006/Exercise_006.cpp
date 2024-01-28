bfe2fdf86af5fdcd90b3c900ecd3888b8aa86a57
David-Kuts <david.mw.kuts@gmail.com>
2024-01-28 22:10:09 +0500

    Add solution of Chapter06_Exercise_006

---
+++ Chapter06_Exercise_006/Exercise_006.cpp
@@ -0,0 +1,61 @@
+// Exercise_006.cpp
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Patrons {
+	std::string name;
+	double donation;
+};
+
+int main() {
+	int patronsAmount = 0;
+
+	std::cout << "Enter a number of patrons: ";
+	std::cin >> patronsAmount;
+
+	Patrons* p_patrons = new Patrons[patronsAmount];
+
+	for (int i = 0; i < patronsAmount; i++) {
+		std::cout << "Enter name: ";
+		std::cin.get();
+		std::getline(std::cin, p_patrons[i].name);
+		std::cout << "Enter donation: ";
+		std::cin >> p_patrons[i].donation;
+	}
+
+	std::vector<int> lowPatronsIndex;
+
+	std::cout << std::endl << "Grand Patrons:" << std::endl;
+
+	bool grandPatronsExist = false;
+	bool patronsExist = false;
+
+	for (int i = 0; i < patronsAmount; i++) {
+		if (p_patrons[i].donation >= 10000) {
+			std::cout << p_patrons[i].name << " - " << p_patrons[i].donation << std::endl;
+			grandPatronsExist = true;
+		}
+		else {
+			lowPatronsIndex.push_back(i);
+			patronsExist = true;
+		}
+	}
+
+	if (!grandPatronsExist) {
+		std::cout << "none" << std::endl;
+	}
+
+	std::cout << std::endl << "Patrons:" << std::endl;
+
+	for (size_t i = 0; i < lowPatronsIndex.size(); i++) {
+		
+		std::cout << p_patrons[lowPatronsIndex[i]].name << " - " << p_patrons[lowPatronsIndex[i]].donation << std::endl;
+	}
+
+	if (!patronsExist) {
+		std::cout << "none" << std::endl;
+	}
+
+	return 0;
+}
