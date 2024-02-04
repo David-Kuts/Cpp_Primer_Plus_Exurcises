c53c3e1981039d36390046b2def27e1183730bea
David-Kuts <david.mw.kuts@gmail.com>
2024-02-04 22:18:27 +0500

    Add solution of Chapter08_Exercise_005

---
+++ Chapter08_Exercise_005/Exercise_005.cpp
@@ -0,0 +1,26 @@
+// Exercise_005.cpp
+#include <iostream>
+
+constexpr int SIZE = 5;
+
+template <typename T>
+T max5(T arr[SIZE]) {
+	T max = arr[0];
+	for (int i = 0; i < SIZE; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+int main() {
+	int numbers1[SIZE] = { 2, 43, 23, 54, 0 };
+	double numbers2[SIZE] = { 2.1, 43.3, 23.5, 54.6, 0.2 };
+
+	std::cout << max5(numbers1) << std::endl;
+	std::cout << max5(numbers2) << std::endl;
+
+
+	return 0;
+}
