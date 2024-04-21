0e54fe040e1340ff52df21409393d60456bf0732
David-Kuts <david.mw.kuts@gmail.com>
2024-04-21 19:52:42 +0500

    Add solution of Chapter16_Exercise_001

---
+++ Chapter16_Exercise_001/Exercise_001.cpp
@@ -0,0 +1,27 @@
+// Exercise_001
+#include<iostream>
+#include<string>
+
+bool isPalindrome(const std::string& str) {
+	std::string::const_iterator left(str.begin());
+	auto right = str.end() - 1;
+	while (left < right) {
+		if (*left != *right) {
+			return false;
+		}
+
+		++left;
+		--right;
+	}
+
+	return true;
+}
+
+int main() {
+	std::cout << "Entre a string : ";
+	std::string input;
+	std::getline(std::cin, input);
+	std::cout << "Is it a palindrome: " << isPalindrome(input) << std::endl;
+
+	return 0;
+}
